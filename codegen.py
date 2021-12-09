f9284ac05e56fba7048ec98f2322783339197bb5
Sai Teja Pratap <sai.teja.pratap@foo.bar>
2021-12-09 11:56:41 -0800

    ok

--- codegen.py
+++ codegen.py
@@ -2,28 +2,42 @@ import sys
 from pathlib import Path
 import uuid
 
-def make_file_content(fname, is_notebook):
-  ret = [fname]
+def make_command(ssize=1000):
   rand_str = lambda: uuid.uuid4().hex
   len_rand_str = len(rand_str())
-  for i in range(1000//len_rand_str):
-      ret.append(rand_str())
+  ret = [rand_str() for i in range(ssize//len_rand_str)]
   all_lines = '\n'.join(ret)
   content = f'"""\n{all_lines}\n"""\n'
-  if is_notebook:
-    content = "# Databricks notebook source\n" + content
-  return content
+  return "# COMMAND ----------\n" + content 
+
+def make_notebook():
+  part1 = "# Databricks notebook source"
+  part2 = "".join((make_command(ssize=200) for _ in range(30)))
+  return part1 + "\n" + part2
+
+def make_file():
+  return make_command()
 
 if __name__ == '__main__':
   nfiles = int(sys.argv[1])
+  nb_ok, files_ok = True, True
+  assert sys.argv[2] in ("files_only", "nb_only", "files_and_nb")
+  if sys.argv[2] == "files_only": nb_ok = False
+  if sys.argv[2] == "nb_only": files_ok = False
+
   for i in range(nfiles):
-    # 123 => 1/2/3.txt
-    fname = '/'.join(list(str(i)))
-    is_notebook = ((i%10) < 2)
-    if is_notebook:
+    if i%10 < 2 and nb_ok:
+      # 123 => 1/2/3_nb.py
+      fname = '/'.join(list(str(i)))
       fname = f"{fname}_nb.py"
-    else:
-      fname = f"{fname}.py"
-    p = Path(fname)
-    p.parent.mkdir(exist_ok=True)
-    p.write_text(make_file_content(fname, is_notebook))
+      p = Path(fname)
+      p.parent.mkdir(exist_ok=True)
+      p.write_text(make_notebook())
+    elif i%10 >= 2 and files_ok:
+      # 123 => 1/2/3.txt
+      fname = '/'.join(list(str(i)))
+      fname = f"{fname}.txt"
+      p = Path(fname)
+      p.parent.mkdir(exist_ok=True)
+      p.write_text(make_file())
+
